61ce6e41329b307ef9f373705af05d1b35cf0a21
Joao Cheixo <jonas4777@gmail.com>
2023-12-14 16:44:09 +0000

    minor changes

---
+++ src/si/__init__.py
@@ -0,0 +1,12 @@
+__author__ = "Joao Cheixo"
+__credits__ = ["Joao Cheixo"]
+__license__ = "Apache License 2.0"
+__version__ = "0.0.1"
+__maintainer__ = "Joao Cheixo"
+__email__ = "jonas4777@gmail.com"
+
+# -----------------------------------------------------------------------------------
+# This package is heavily inspired and adapted from
+# - https://github.com/vmspereira/si
+# - https://github.com/cruz-f/si
+# -----------------------------------------------------------------------------------
